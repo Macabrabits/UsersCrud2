51fdeb3602464d86a75897d5e1b9e76ed6548e74
Macabrabits <vitor_xd_7@hotmail.com>
2019-03-16 15:03:54 -0300

    Organização do projeto

---
+++ frontend/src/components/template/Main.jsx
@@ -0,0 +1,12 @@
+import './Main.css'
+import React ,{Fragment} from 'react'
+import Header from './Header'
+
+export default props =>
+<Fragment>
+    <Header>
+        <main className="content">
+            Conteúdo
+        </main>
+    </Header>
+</Fragment>
