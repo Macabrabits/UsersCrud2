fe6e2ec50df101f2b3de0bd15f4e87798d6ec84d
Macabrabits <vitor_xd_7@hotmail.com>
2019-03-23 00:18:09 -0300

    Finalização de funções de 'UserCrud'

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -18,11 +18,15 @@ export default class UserCrud extends Component {
 
     state = { ...initialState }
 
+    componentWillMount() {
+        axios(baseUrl).then(resp => {
+            this.setState({ list: resp.data })
+        })
+    }
+
     clear() {
         this.setState({ user: initialState.user })
     }
-
-
 
     save() {
         const user = this.state.user
@@ -87,11 +91,61 @@ export default class UserCrud extends Component {
         )
     }
 
+    load(user) {
+        this.setState({ user })
+    }
+
+    remove(user) {
+        axios.delete(`${baseUrl}/${user.id}`).then(resp => {
+            const list = this.state.list.filter(u => u !== user)
+            this.setState({ list })
+        })
+    }
+
+    renderTable() {
+        return (
+            <table className="table mt-4">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.renderRows()}
+                </tbody>
+                
+            </table>
+        )
+
+    }
+
+    renderRows() {
+        return this.state.list.map(user => {
+            return (
+                <tr key={user.id}>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>
+                        <button className="btn btn-warning" onClick={() => this.load(user)}>
+                            <i className="fa fa-pencil"></i>
+                        </button>
+                        <button onClick={() => this.remove(user)} className="btn btn-danger ml-2"><i className="fa fa-trash"></i></button>
+                    </td>
+                </tr>
+
+            )
+        })
+    }
+
 
     render() {
+        console.log(this.state.list)
         return (
             <Main {...headerProps}>
                 {this.renderForm()}
+                {this.renderTable()}
             </Main>
         )
     }
